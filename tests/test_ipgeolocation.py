46ff5016d61816d5b80b83db00201d9ea365fe7c
ip2location <support@ip2location.com>
2024-08-22 07:55:07 +0800

    Fixed one pytest issue.

--- tests/test_ipgeolocation.py
+++ tests/test_ipgeolocation.py
@@ -4,16 +4,16 @@ import pytest
 
 import ip2locationio
 
-def testinvalidapikey():
-    configuration = ip2locationio.Configuration('')
+def testinvalidapikey(global_data):
+    configuration = ip2locationio.Configuration(global_data["apikey"])
     ipgeolocation = ip2locationio.IPGeolocation(configuration)
     try:
         result = ipgeolocation.lookup('8.8.8.8')
     except IP2LocationIOAPIError as e:
         assert str(e) == 'Invalid API key or insufficient credit.'
 
-def testfunctionexist():
-    configuration = ip2locationio.Configuration('')
+def testfunctionexist(global_data):
+    configuration = ip2locationio.Configuration(global_data["apikey"])
     ipgeolocation = ip2locationio.IPGeolocation(configuration)
     functions_list = ['lookup']
     for x in range(len(functions_list)): 
